c15792cd28764e966948182bab7940cc65a8505d
YJH <yoojh96@naver.com>
2020-12-23 11:44:35 +0900

    네스트

--- app/src/main/java/com/example/git_test/MainActivity.kt
+++ app/src/main/java/com/example/git_test/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//      이것은 유주희씨가  작업한것입니다.
     }
 }
